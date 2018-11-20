c25f795cd34eda51474145a4c7878059a8f24979
Daniel Santana Santos <gc04346@gmail.com>
2018-11-20 16:31:57 -0200

    initial commit

---
+++ haskell.hs
@@ -0,0 +1,34 @@
+pow :: Integer -> Integer -> Integer
+pow n e
+ |e==1 = n
+ |otherwise = n * pow n (e-1)
+ 
+somaImpares :: [Integer] -> Integer
+somaImpares [] = 0
+somaImpares (a:lista)
+ |a `mod` 2 == 0 = 0 + somaImpares lista
+ |otherwise = a + somaImpares lista
+
+substituir :: Integer -> Integer -> [Integer] -> [Integer]
+substituir sub new [] = []
+substituir sub new (a:lista)
+ |a == sub = new : substituir sub new lista
+ |otherwise = [a] ++ substituir sub new lista
+
+verificaPrimo :: Integer -> [Integer] -> Bool
+verificaPrimo n [] = True
+verificaPrimo n (a:lista)
+ |n `mod` a == 0 = False
+ |otherwise = verificaPrimo n lista
+
+primo :: Integer -> Bool
+primo n
+ |n == 0 = False
+ |n == 1 = True
+ |n == 2 = True
+ |otherwise = verificaPrimo n [2..(n-1)]
+ 
+
+
+main = do
+    print $ primo 997
